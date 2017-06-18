c1b0e847cd7bf648fbdfacf78f9bc0a396d09fa7
dixita-developer <dixirana.1991@gmail.com>
2017-06-18 17:39:16 -0400

    First upload of project

---
+++ Project1/com/pkg/MainClass.java
@@ -0,0 +1,13 @@
+package com.pkg;
+
+public class MainClass {
+
+	public MainClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Testing Git");
+	}
+
+}
